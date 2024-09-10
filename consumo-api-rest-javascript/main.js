70c7cc3304cf529356cd0c74eb2dea8117c2b17e
Irving mtz <al2173074687@azc.uam.mx>
2024-09-10 00:05:57 -0600

    uso de endpoints y query parameters, en el consumo de una API

--- consumo-api-rest-javascript/main.js
+++ consumo-api-rest-javascript/main.js
@@ -14,20 +14,29 @@
 //     window.location.reload()
 // })
 
-const urlApi = 'https://api.thecatapi.com/v1/images/search'
+const urlApi = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
-async function fetchData(urlApi){
+async function reload(urlApi){
     const response = await fetch(urlApi);
     const data = await response.json();
-    const img = document.querySelector('img')
-    img.src = data[0].url
+    const img1 = document.getElementById('img1')
+    const img2 = document.getElementById('img2')
+    const img3 = document.getElementById('img3')
+
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+    console.log(data)
+    // img.src = data[0].url
 }
 
 const button = document.querySelector('button')
-
-document.addEventListener('DOMContentLoaded',fetchData(urlApi))
+// document.addEventListener('DOMContentLoaded',fetchData(urlApi))
 
 button.addEventListener('click',()=>{
-    fetchData(urlApi);
+    reload(urlApi);
     windows.location.reload();
 })
+
+// Se manda a llamar la función cuando recien se carga la página
+reload(urlApi);
