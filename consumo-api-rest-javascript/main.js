5ccbcfb8143dd241c7d6e0fbd798d273ecda8a80
Irving mtz <al2173074687@azc.uam.mx>
2024-09-10 23:05:31 -0600

    uso de get para obtener los michis favoritos

--- consumo-api-rest-javascript/main.js
+++ consumo-api-rest-javascript/main.js
@@ -1,42 +1,44 @@
-// console.log('Hello, world')
+const urlApi_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_EAiOhGE3wh3dcvwYQxxwZ7X5N3zYG286hEFBNu5jjFYnwYNxYNA07tCpg9XTOGGT';
 
-// const URL = 'https://api.thecatapi.com/v1/images/search'
-// const buttonR = document.querySelector("button")
+const urlApi_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_EAiOhGE3wh3dcvwYQxxwZ7X5N3zYG286hEFBNu5jjFYnwYNxYNA07tCpg9XTOGGT';
 
-// fetch(URL)
-// .then(res => res.json())
-// .then(data =>{
-//     const img = document.querySelector("img")
-//     img.src = data[0].url
-// })
+const spanError = document.getElementById('error')
 
-// buttonR.addEventListener("click",()=>{
-//     window.location.reload()
-// })
-
-const urlApi = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_EAiOhGE3wh3dcvwYQxxwZ7X5N3zYG286hEFBNu5jjFYnwYNxYNA07tCpg9XTOGGT';
-
-async function reload(urlApi){
-    const response = await fetch(urlApi);
+async function loadRandomMichis(){
+    const response = await fetch(urlApi_RANDOM);
     const data = await response.json();
-    const img1 = document.getElementById('img1')
-    const img2 = document.getElementById('img2')
-    const img3 = document.getElementById('img3')
-
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+    console.log("Random")
     console.log(data)
-    // img.src = data[0].url
-}
 
+    if(response.status !== 200){
+        spanError.innerHTML = "Hubo un error: "+ response.status + " / " + data.message;
+    }else{
+        const img1 = document.getElementById('img1')
+        const img2 = document.getElementById('img2')
+    
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+    }
+}
+async function loadFavoritesMichis(){
+    const response = await fetch(urlApi_FAVORITES);
+    const data = await response.json();
+
+    console.log("Favorites")
+    console.log(data)
+
+    if(response.status !== 200){
+        spanError.innerHTML = "Hubo un error : "+ response.status + " / " + data.message;
+    }
+}
 const button = document.querySelector('button')
 // document.addEventListener('DOMContentLoaded',fetchData(urlApi))
 
 button.addEventListener('click',()=>{
-    reload(urlApi);
+    loadRandomMichis();
     window.location.reload();
 })
 
 // Se manda a llamar la función cuando recien se carga la página
-reload(urlApi);
+loadRandomMichis();
+loadFavoritesMichis()
