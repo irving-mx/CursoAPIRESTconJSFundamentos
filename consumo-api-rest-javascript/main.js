12de748c1229efbcfe17cfad92d9fd33a79bb385
Irving mtz <al2173074687@azc.uam.mx>
2024-09-11 03:56:48 -0600

    uso del metodo POST para guardar michis favoritos

--- consumo-api-rest-javascript/main.js
+++ consumo-api-rest-javascript/main.js
@@ -1,6 +1,6 @@
 const urlApi_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_EAiOhGE3wh3dcvwYQxxwZ7X5N3zYG286hEFBNu5jjFYnwYNxYNA07tCpg9XTOGGT';
 
-const urlApi_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_EAiOhGE3wh3dcvwYQxxwZ7X5N3zYG286hEFBNu5jjFYnwYNxYNA07tCpg9XTOGGT';
+const urlApi_FAVORITES = 'https://api.thecatapi.com/v1/favourites?api_key=live_EAiOhGE3wh3dcvwYQxxwZ7X5N3zYG286hEFBNu5jjFYnwYNxYNA07tCpg9XTOGGT';
 
 const spanError = document.getElementById('error')
 
@@ -11,16 +11,16 @@ async function loadRandomMichis(){
     console.log(data)
 
     if(response.status !== 200){
-        spanError.innerHTML = "Hubo un error: "+ response.status + " / " + data.message;
+        spanError.innerHTML = "Hubo un error : "+ response.status + " / " + data.message;
     }else{
-        const img1 = document.getElementById('img1')
-        const img2 = document.getElementById('img2')
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
     
         img1.src = data[0].url;
         img2.src = data[1].url;
     }
 }
-async function loadFavoritesMichis(){
+async function loadFavouriteMichis(){
     const response = await fetch(urlApi_FAVORITES);
     const data = await response.json();
 
@@ -31,14 +31,44 @@ async function loadFavoritesMichis(){
         spanError.innerHTML = "Hubo un error : "+ response.status + " / " + data.message;
     }
 }
-const button = document.querySelector('button')
+// const button = document.querySelector('button')
 // document.addEventListener('DOMContentLoaded',fetchData(urlApi))
 
-button.addEventListener('click',()=>{
-    loadRandomMichis();
-    window.location.reload();
-})
+// button.addEventListener('click',()=>{
+//     loadRandomMichis();
+//     window.location.reload();
+// })
+
+async function saveFavouriteMichis(){
+    const res = await fetch(urlApi_FAVORITES,
+        {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                image_id: 'nube'
+            }),
+        }
+    );
+    const data = await res.json();
+    
+    console.log('Save')
+    console.log(data)
+
+    if(res.status !== 200){
+        spanError.innerHTML = "Hubo un error : "+ res.status + " / " + data.message;
+    }
+}
+
+
+
+
 
 // Se manda a llamar la función cuando recien se carga la página
 loadRandomMichis();
-loadFavoritesMichis()
+loadFavouriteMichis()
+
+
+
+
